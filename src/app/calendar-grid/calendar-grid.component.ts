3619f35c55ee663dfe11e343f2bc5c428dcece56
Leonello Leone <leonellodeleone@yahoo.com.br>
2018-10-04 21:35:35 -0300

    Backup

--- src/app/calendar-grid/calendar-grid.component.ts
+++ src/app/calendar-grid/calendar-grid.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { AppComponent } from "../app.component";
+import { Event } from "../event";
 
 @Component({
   selector: 'app-calendar-grid',
@@ -8,12 +9,22 @@ import { AppComponent } from "../app.component";
 })
 export class CalendarGridComponent implements OnInit {
   @Input() turno: string;
+  public salas;
 
   constructor(private app: AppComponent) {
+    this.salas = app.salas;
   }
 
   ngOnInit() {
-    //console.log(this.app);
+    //setTimeout( ()=>console.log(this.app.events), 1000);
   }
 
+  checkCell(i: number, sala: string, turno: string) {
+    let filtered = this.app.events.filter( ev => {
+      return this.app.testEventDate(ev, this.app.weekDates[i])
+          && this.app.testEventSala(ev, sala)
+          && this.app.testEventTurno(ev, turno);
+    }, this);
+    return filtered.length == 1 ? filtered[0].curso : filtered.length;
+  }
 }
